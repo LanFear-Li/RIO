3e29d7f1e6f7130616db36c41430c85d2be09699
LanFear <1371705749@qq.com>
2024-08-31 17:02:13 +0800

    Feature: Add scene loading with command line option

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "renderer.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-    Renderer renderer("default-scene");
+    auto scene_name = "default-scene";
+
+    if (argc == 2) {
+        scene_name = argv[1];
+    }
+
+    Renderer renderer(scene_name);
     renderer.run();
     return 0;
 }
