263be59f542366bbc7d27eb324cdfeb5999a7a20
LanFear <1371705749@qq.com>
2024-08-31 21:39:50 +0800

    Feature: Update camera instantly after reset

--- src/scene/camera.cpp
+++ src/scene/camera.cpp
@@ -109,15 +109,16 @@ void Camera::ProcessMouseScroll(float y_offset)
 
 void Camera::updateCameraVectors()
 {
-    // calculate the new Front vector
+    // Calculate the new Front vector.
     glm::vec3 front;
     front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
     front.y = sin(glm::radians(Pitch));
     front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
     Front = glm::normalize(front);
-    // also re-calculate the Right and Up vector
-    Right = glm::normalize(glm::cross(Front,
-                                        WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+
+    // Also re-calculate the Right and Up vector.
+    // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+    Right = glm::normalize(glm::cross(Front, WorldUp));
     Up = glm::normalize(glm::cross(Right, Front));
 }
 
@@ -126,4 +127,7 @@ void Camera::ResetCamera()
     Position = glm::vec3{0.0, 0.0, 5.0};
     Yaw = -90.0f;
     Pitch = 0.0f;
+
+    // Update camera matrix after reset.
+    updateCameraVectors();
 }
